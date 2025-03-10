fd9953474aad0ac9db6571aa51d06d52bfd28892
vlaszdunov <zdunovvl@gmail.com>
2025-03-10 16:48:00 +0300

    add method to get available OS for cloud servers in cloud_server_manager.py

--- src/timeweb_sdk/managers/cloud_server_manager.py
+++ src/timeweb_sdk/managers/cloud_server_manager.py
@@ -3,18 +3,20 @@ from typing import Optional
 
 
 class CloudServerManager(_Base):
-    __base_endpoint = "/servers"
+    __root_url = "https://api.timeweb.cloud/api/v1"
+    __base_endpoint = f"{__root_url}/servers"
     __get_servers = {"method": "get", "endpoint": __base_endpoint}
     __get_server = {"method": "get", "endpoint": __base_endpoint}
+    __get_os = {"method": "get", "endpoint": f"{__root_url}/os/servers"}
 
     def __init__(self, access_token):
         super().__init__(access_token)
-        self.__base_endpoint = self._base_url + self.__base_endpoint
-        self.__get_servers["endpoint"]: str = self.__base_endpoint
-        self.__get_server["endpoint"]: str = self.__base_endpoint
 
     def get_list_of_servers(self):
         return self.make_request(self.__get_servers["method"], self.__get_servers["endpoint"])
 
     def get_server_by_id(self, server_id: int):
         return self.make_request(self.__get_server["method"], self.__get_servers["endpoint"] + f"/{server_id}")
+
+    def get_os(self):
+        return self.make_request(self.__get_os["method"], self.__get_os["endpoint"])
