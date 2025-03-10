8cee9d19d839e6ed94ff2e8e59fc79978657480f
vlaszdunov <zdunovvl@gmail.com>
2025-03-10 13:28:23 +0300

    add method to get specific cloud server by id

--- src/timeweb_sdk/managers/cloud_server_manager.py
+++ src/timeweb_sdk/managers/cloud_server_manager.py
@@ -5,11 +5,16 @@ from typing import Optional
 class CloudServerManager(_Base):
     __base_endpoint = "/servers"
     __get_servers = {"method": "get", "endpoint": __base_endpoint}
+    __get_server = {"method": "get", "endpoint": __base_endpoint}
 
     def __init__(self, access_token):
         super().__init__(access_token)
         self.__base_endpoint = self._base_url + self.__base_endpoint
         self.__get_servers["endpoint"]: str = self.__base_endpoint
+        self.__get_server["endpoint"]: str = self.__base_endpoint
 
     def get_list_of_servers(self):
         return self.make_request(self.__get_servers["method"], self.__get_servers["endpoint"])
+
+    def get_server_by_id(self, server_id: int):
+        return self.make_request(self.__get_server["method"], self.__get_servers["endpoint"] + f"/{server_id}")
