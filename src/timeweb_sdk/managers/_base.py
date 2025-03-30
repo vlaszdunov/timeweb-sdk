2a26b752c91613142cf563fdf1563da55b9af278
vlaszdunov <zdunovvl@gmail.com>
2025-03-30 09:52:24 +0300

    add methods: clone_server, shutdown_server, reset_password, start, reboot

--- src/timeweb_sdk/managers/_base.py
+++ src/timeweb_sdk/managers/_base.py
@@ -1,28 +1,28 @@
-from typing import Optional
+from typing import Optional, Literal
 
 from timeweb_sdk.utils import BearerAuth
-from httpx import get, post
+from httpx import get, post, delete, put
 
 
 class _Base:
     def __init__(self, access_token):
         self.__access_token = access_token
 
-    def make_request(self, method: str, endpoint, data: dict = Optional[None]):
+    def _make_request(self, method: Literal["get", "post"], endpoint, data: dict = Optional[None]):
         match method:
             case "get":
-                return self._get(endpoint)
+                return self.__get(endpoint)
             case "post":
-                return self._post(endpoint, data)
+                return self.__post(endpoint, data)
 
-    def _get(self, endpoint):
+    def __get(self, endpoint):
         return get(
             url=endpoint,
             auth=BearerAuth(self.__access_token),
             headers={"Content-Type": "application/json"},
         )
 
-    def _post(self, endpoint, data):
+    def __post(self, endpoint, data):
         return post(
             url=endpoint,
             json=data,
--- src/timeweb_sdk/managers/cloud_server_manager.py
+++ src/timeweb_sdk/managers/cloud_server_manager.py
@@ -1,106 +1,127 @@
 from timeweb_sdk.managers._base import _Base
+from typing import Literal
+from warnings import deprecated
 
 
 class CloudServerManager(_Base):
     __root_url = "https://api.timeweb.cloud/api/v1"
     __base_endpoint = f"{__root_url}/servers"
-    __get_servers = {"method": "get", "endpoint": __base_endpoint}
-    __get_server = {"method": "get", "endpoint": f"{__base_endpoint}/server_id"}
-    __get_os = {"method": "get", "endpoint": f"{__root_url}/os/servers"}
-    __get_presets = {"method": "get", "endpoint": f"{__root_url}/presets/servers"}
-    __get_configs = {"method": "get", "endpoint": f"{__root_url}/configurator/servers"}
-    __get_software = {"method": "get", "endpoint": f"{__root_url}/software/server"}
-    __get_server_ips = {"method": "get", "endpoint": f"{__base_endpoint}/server_id/ips"}
-    __get_server_logs = {"method": "get", "endpoint": f"{__base_endpoint}/server_id/logs"}
-    __get_server_drives = {"method": "get", "endpoint": f"{__base_endpoint}/server_id/disks"}
-    __get_server_drive_by_id = {"method": "get", "endpoint": f"{__base_endpoint}/server_id/disks/drive_id"}
-    __get_drive_autobackup_settings = {
-        "method": "get",
-        "endpoint": f"{__base_endpoint}/server_id/disks/drive_id/auto-backups",
-    }
-    __get_all_drive_backups = {"method": "get", "endpoint": f"{__base_endpoint}/server_id/disks/drive_id/backups"}
-    __get_drive_backup_by_id = {
-        "method": "get",
-        "endpoint": f"{__base_endpoint}/server_id/disks/drive_id/backups/backup_id",
-    }
-    __shutdown_server = {"method": "post", "endpoint": f"{__base_endpoint}/server_id/shutdown"}
 
     def __init__(self, access_token):
         super().__init__(access_token)
+        self.__access_token = access_token
 
     def get_list_of_servers(self):
-        return self.make_request(self.__get_servers["method"], self.__get_servers["endpoint"])
-
-    def get_server_by_id(self, server_id: int):
-        return self.make_request(
-            self.__get_server["method"], self.__get_servers["endpoint"].replace("server_id", str(server_id))
+        return self._make_request(
+            "get",
+            self.__base_endpoint,
         )
 
+    def get_server_by_id(self, server_id: int):
+        return self._make_request(
+            "get",
+            f"{self.__base_endpoint}/{server_id}",
+        )
+
+    @deprecated("Deprecated! Use methods like ``shutdown_server()`` instead.")
+    def execute_server_action(
+        self,
+        server_id: int,
+        action: Literal[
+            "hard_reboot",
+            "hard_shutdown",
+            "install",
+            "reboot",
+            "remove",
+            "reset_password",
+            "shutdown",
+            "start",
+            "clone",
+        ],
+    ):
+        data = {"action": action}
+        return self._make_request("post", f"{self.__base_endpoint}/{server_id}/action", data)
+
+    def clone_server(self, server_id: int):
+        return self._make_request("post", f"{self.__base_endpoint}/{server_id}/clone")
+
+    def hard_shutdown_server(self, server_id: int):
+        return self._make_request("post", f"{self.__base_endpoint}/{server_id}/hard-shutdown")
+
+    def shutdown_server(self, server_id: int):
+        return self._make_request("post", f"{self.__base_endpoint}/{server_id}/shutdown")
+
+    def reset_server_password(self, server_id: int):
+        return self._make_request("post", f"{self.__base_endpoint}/{server_id}/reset-password")
+
+    def start_server(self, server_id: int):
+        return self._make_request("post", f"{self.__base_endpoint}/{server_id}/start")
+
+    def reboot_server(self, server_id: int):
+        return self._make_request("post", f"{self.__base_endpoint}/{server_id}/reboot")
+
     def get_os(self):
-        return self.make_request(self.__get_os["method"], self.__get_os["endpoint"])
+        return self._make_request(
+            "get",
+            f"{self.__root_url}/os/servers",
+        )
 
     def get_server_presets(self):
-        return self.make_request(self.__get_presets["method"], self.__get_presets["endpoint"])
+        return self._make_request(
+            "get",
+            f"{self.__root_url}/presets/servers",
+        )
 
     def get_server_configs(self):
-        return self.make_request(self.__get_configs["method"], self.__get_configs["endpoint"])
+        return self._make_request(
+            "get",
+            f"{self.__root_url}/configurator/servers",
+        )
 
     def get_available_software(self):
-        return self.make_request(self.__get_software["method"], self.__get_software["endpoint"])
+        return self._make_request(
+            "get",
+            f"{self.__root_url}/software/servers",
+        )
 
     def get_server_ips(self, server_id: int):
-        return self.make_request(
-            self.__get_server_ips["method"],
-            self.__get_server_ips["endpoint"].replace("server_id", str(server_id)),
+        return self._make_request(
+            "get",
+            f"{self.__base_endpoint}/{server_id}/ips",
         )
 
     def get_server_logs(self, server_id: int):
-        return self.make_request(
-            self.__get_server_logs["method"],
-            self.__get_server_logs["endpoint"].replace("server_id", str(server_id)),
+        return self._make_request(
+            "get",
+            f"{self.__base_endpoint}/{server_id}/logs",
         )
 
     def get_server_drives(self, server_id: int):
-        return self.make_request(
-            self.__get_server_drives["method"],
-            self.__get_server_drives["endpoint"].replace("server_id", str(server_id)),
+        return self._make_request(
+            "get",
+            f"{self.__base_endpoint}/{server_id}/disks",
         )
 
     def get_server_drive_by_id(self, server_id: int, drive_id: int):
-        return self.make_request(
-            self.__get_server_drive_by_id["method"],
-            self.__get_server_drive_by_id["endpoint"]
-            .replace("server_id", str(server_id))
-            .replace("drive_id", str(drive_id)),
+        return self._make_request(
+            "get",
+            f"{self.__base_endpoint}/{server_id}/disks/{drive_id}",
         )
 
     def get_drive_backup_settings(self, server_id: int, drive_id: int):
-        return self.make_request(
-            self.__get_drive_autobackup_settings["method"],
-            self.__get_drive_autobackup_settings["endpoint"]
-            .replace("server_id", str(server_id))
-            .replace("drive_id", str(drive_id)),
+        return self._make_request(
+            "get",
+            f"{self.__base_endpoint}/{server_id}/disks/{drive_id}/auto-backups",
         )
 
     def get_all_drive_backups(self, server_id: int, drive_id: int):
-        return self.make_request(
-            self.__get_all_drive_backups["method"],
-            self.__get_all_drive_backups["endpoint"]
-            .replace("server_id", str(server_id))
-            .replace("drive_id", str(drive_id)),
+        return self._make_request(
+            "get",
+            f"{self.__base_endpoint}/{server_id}/disks/{drive_id}/backups",
         )
 
     def get_drive_backup_by_id(self, server_id: int, drive_id: int, backup_id: int):
-        return self.make_request(
-            self.__get_drive_backup_by_id["method"],
-            self.__get_drive_backup_by_id["endpoint"]
-            .replace("server_id", str(server_id))
-            .replace("drive_id", str(drive_id))
-            .replace("backup_id", str(backup_id)),
-        )
-
-    def shutdown_server(self, server_id: int):
-        return self.make_request(
-            self.__shutdown_server["method"],
-            self.__shutdown_server["endpoint"].replace("server_id", str(server_id)),
+        return self._make_request(
+            "get",
+            f"{self.__base_endpoint}/{server_id}/disks/{drive_id}/backups/{backup_id}",
         )
