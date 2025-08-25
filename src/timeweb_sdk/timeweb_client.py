33281c01af460c18c722938a0b353d44de3d7aab
vlaszdunov <zdunovvl@gmail.com>
2025-08-25 15:08:12 +0300

    add BaseClient class, that will be sending requests, validate API token and responses

--- src/timeweb_sdk/timeweb_client.py
+++ src/timeweb_sdk/timeweb_client.py
@@ -1,9 +1,9 @@
 from httpx import Client
-from jwt import InvalidTokenError
+from utils.base_client import BaseClient
 
-from utils import BearerAuth
-from utils.exceptions import *
-from managers import CloudServerManager
+from .utils import BearerAuth
+from .utils.exceptions import *
+from .managers import CloudServerManager
 import jwt
 
 
@@ -17,40 +17,14 @@ class TimewebCloudClient:
     """
 
     __access_token: str
-    __client: Client
     cloud_server_manager: CloudServerManager
 
     def __init__(self, access_token: str) -> None:
-        self.__verify_token(access_token)
-        self.__access_token = access_token
-        self.__client = Client(
-            auth=BearerAuth(self.__access_token),
-            base_url="https://access.timeweb.cloud/api/v1",
-            headers={"Content-Type": "application/json"},
-            http2=True,
-        )
-        self.cloud_server_manager = CloudServerManager(self.__access_token, client=self.__client)
-
-    def close_client(self):
-        self.__client.close()
-
-    def __verify_token(self, access_token: str):
-        try:
-            jwt.decode(
-                access_token,
-                options={
-                    "verify_signature": False,
-                    "verify_exp": True,
-                    "verify_nbf": True,
-                },
-            )
-        except jwt.exceptions.ExpiredSignatureError:
-            raise ExpiredAccessTokenError() from None
-        except jwt.exceptions.InvalidTokenError or jwt.exceptions.ImmatureSignatureError:
-            raise InvalidToken() from None
+        self.__client = BaseClient(access_token)
+        self.cloud_server_manager = CloudServerManager(client=self.__client)
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-        self.close_client()
+        self.__client.close_client()
---
+++ src/timeweb_sdk/utils/base_client.py
@@ -0,0 +1,91 @@
+import json
+from collections import OrderedDict
+from typing import Any
+
+from timeweb_sdk.utils.exceptions import *
+from timeweb_sdk.utils import BearerAuth
+from httpx import Client, Response, codes
+import jwt
+
+__all__ = ["BaseClient"]
+
+
+class BaseClient:
+    """
+    Args:
+        access_token (str): Timeweb Cloud Bearer API token
+    """
+
+    def __init__(self, access_token: str) -> None:
+        self.__verify_token(access_token)
+        self.__client = Client(
+            auth=BearerAuth(access_token),
+            base_url="https://access.timeweb.cloud/api/v1",
+            headers={"Content-Type": "application/json"},
+            http2=True,
+        )
+
+    def close_client(self) -> None:
+        self.__client.close()
+
+    def __verify_token(self, access_token: str) -> None:
+        """
+        Validates the access token
+
+        Args:
+            access_token (str): Timeweb Cloud Bearer API token
+
+        Raises:
+            ExpiredAccessTokenError: If the access token is expired
+            InvalidToken: If the access token is invalid
+        """
+        try:
+            jwt.decode(
+                access_token,
+                options={
+                    "verify_signature": False,
+                    "verify_exp": True,
+                    "verify_nbf": True,
+                },
+            )
+        except jwt.exceptions.ExpiredSignatureError:
+            raise ExpiredAccessTokenError() from None
+        except jwt.exceptions.InvalidTokenError or jwt.exceptions.ImmatureSignatureError:
+            raise InvalidToken() from None
+
+    def get(self, url: str, params: dict | None = None) -> dict:
+        response = self.__client.get(url=url, params=params)
+        self.__validate_http_response(response)
+        return response.json()
+
+    def post(self, url: str, data: dict | None = None) -> dict:
+        response = self.__client.post(url=url, data=data)
+        self.__validate_http_response(response)
+        return response.json()
+
+    def delete(self, url: str, params: dict | None = None) -> dict:
+        response = self.__client.delete(url=url, params=params)
+        self.__validate_http_response(response)
+        return response.json()
+
+    def __validate_http_response(self, response: Response) -> Any | None:
+        match response.status_code:
+            case codes.OK | codes.CREATED:
+                return json.loads(response.text, object_hook=lambda pairs: OrderedDict(pairs))
+            case codes.NO_CONTENT:
+                return None
+            case codes.BAD_REQUEST:
+                raise HTTPBadRequestError(response.json()["message"])
+            case codes.UNAUTHORIZED:
+                raise HTTPUnauthorizedError(response.json()["message"])
+            case codes.FORBIDDEN:
+                raise HTTPForbiddenError(response.json()["message"])
+            case codes.NOT_FOUND:
+                raise HTTPNotFoundError(response.json()["message"])
+            case codes.TOO_MANY_REQUESTS:
+                raise HTTPTooManyRequestsError(response.json()["message"])
+            case codes.INTERNAL_SERVER_ERROR:
+                raise HTTPInternalServerError(response.json()["message"])
+            case codes.LOCKED:
+                raise HTTPLockedError(response.json()["message"])
+        return None
